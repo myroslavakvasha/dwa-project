fb28f865fc6c12d823dfcc98fab05d383e130638
myroslavakvasha <mkvasha@algebra.hr>
2026-06-25 17:18:48 +0200

    added untracked Logs T_T

---
+++ BL/DTOs/Log/LogResponseDto.cs
@@ -0,0 +1,10 @@
+﻿namespace BL.DTOs.Log
+{
+    public class LogResponseDto
+    {
+        public int Id { get; set; }
+        public DateTime Timestamp { get; set; }
+        public string Message { get; set; } = null!;
+        public string LogLevelTitle { get; set; } = null!;
+    }
+}
